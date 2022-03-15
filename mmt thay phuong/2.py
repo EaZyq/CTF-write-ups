51f15be69738bed2a36a09076e7cc23dd325510a
EaZyq <70899816+EaZyq@users.noreply.github.com>
2022-03-15 15:41:19 +0700

    Update 2.py

--- mmt thay phuong/2.py
+++ mmt thay phuong/2.py
@@ -17,14 +17,10 @@ while(1):
     m = int(bytearray.fromhex(a[12:16].hex())[::-1].hex(), 16)
     x = int(bytearray.fromhex(a[16:20].hex())[::-1].hex(), 16)
     p = 0
-    print(hex(n))
-    print(hex(m))
-    print(hex(x))
     for i in range(0, n + 1):
         A = int(bytearray.fromhex(a[20+i*4:24+i*4].hex())[::-1].hex(), 16)
         p += A * (pow(x, i))
     result = p % m 
-    print(hex(result))
     payload = p32(0x2)
     payload += p32(0x4)
     payload += p32(result)
